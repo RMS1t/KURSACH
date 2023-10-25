47e457b49fcba0b7cb8ebbe1d43369c9eca94906
unknown <locadm@O401-7.ttit.local>
2023-10-25 14:30:22 +0700

    Настройка окружения с возможностью подключения к бд + архитектура приложения

---
+++ myproject.loc/src/MyProject/Models/Users/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MyProject\Models\Users;
+
+use MyProject\Models\ActiveRecordEntity;
+
+class User extends ActiveRecordEntity
+{
+     
+    protected string $nickname;
+
+    protected string $email;
+
+   protected string $isConfirmed;
+
+    protected string $role;
+
+   protected string $passwordHash;
+
+   protected string $authToken;
+
+   protected string $createdAt;
+
+    public function getNickname(): string
+    {
+        return $this->nickname;
+    }
+
+    protected static function getTableName(): string
+    {
+        return 'users';
+    }
+}
+
