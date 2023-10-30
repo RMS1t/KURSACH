d059e0d2ff860858ec2b9ba98f7428e32f4c2eae
Роман Максимов <gr412_marst@lab-407-1.tomtit-tomsk.ru>
2023-10-30 14:16:16 +0700

    Mm-umu-Install  composer, but not update

---
+++ pop-it-mvc/composer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rms1t/gryschewsky_style',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rms1t/gryschewsky_style' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
