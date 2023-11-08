ce9c91e5c4cef1494f78a3ef9da8e6be613bf607
unknown <locadm@O401-3.ttit.local>
2023-11-08 16:32:43 +0700

    reload laravel

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return 'hola';
     return view('welcome');
+});
+Route::get('/hh', function () {
+    return 'hola1';
+  
 });
